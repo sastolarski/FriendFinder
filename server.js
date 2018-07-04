823ff66735d018e8b4910a2161cfec5afa3cb2ed
sastolarski <sastolarski@gmail.com>
2018-07-04 10:49:36 -0500

    changed survey added more routes

--- server.js
+++ server.js
@@ -12,19 +12,27 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//express routes to things in public that match queries
+app.use(express.static("public")); 
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("http://localhost:" + PORT);
-  });
-
-  app.get("/", function(req, res) {
-    res.sendFile('public/home.html' , { root : __dirname});
 });
 
-app.get("/survey", function(req, res){
-    res.sendFile('public/survey.html' , { root : __dirname});
+app.get("/", function (req, res) {
+    res.sendFile('public/home.html', { root: __dirname });
 });
+
+app.get("/survey", function (req, res) {
+    res.sendFile('public/survey.html', { root: __dirname });
+});
+
+app.get("/api/friends", function(req,res){
+    res.sendFile("app/data/friends.js", { root: __dirname });
+})
+
