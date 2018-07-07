482999b45ca7d0dcdc9b03e64e88833a356d491c
sastolarski <sastolarski@gmail.com>
2018-07-07 09:55:13 -0500

    more

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,28 @@
 
 var path = require('path');
+var friends = require('../data/friends.js');
 
 module.exports = function (app) {
 
-    // HTML GET Requests
 
+
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
 
     app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname + '/../../public/survey.html'));
     });
-
-    // If no matching route is found default to home
+    
 
     app.use(function (req, res) {
         res.sendFile(path.join(__dirname + '/../../public/home.html'));
     });
 
+    app.post('/api/friends', function(req, res) {
+		
+        var userInput = req.body;
+        
+        friends.push(userInput);
+    })
 }
